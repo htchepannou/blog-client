1c004d9934cbc1c8bbe71f9d0d914111b24cb68c
htchepannou <htchepannou@expedia.com>
2015-08-29 11:17:25 -0400

    rename events

--- src/main/java/com/tchepannou/blog/client/v1/BlogConstants.java
+++ src/main/java/com/tchepannou/blog/client/v1/BlogConstants.java
@@ -9,8 +9,8 @@ public class BlogConstants {
     public static final String METRIC_DELETE_POST = "blog-delete-post";
 
     public static final String QUEUE_EVENT_LOG = "blog-queue-event-log";
-    public static final String EVENT_CREATE_POST = "blog-create-post-post";
-    public static final String EVENT_UPDATE_POST = "blog-update-post-post";
+    public static final String EVENT_CREATE_POST = "blog-create-post";
+    public static final String EVENT_UPDATE_POST = "blog-update-post";
     public static final String EVENT_DELETE_POST = "blog-deleted-post";
     public static final String EVENT_REBLOG_POST = "blog-reblog-post";
 
