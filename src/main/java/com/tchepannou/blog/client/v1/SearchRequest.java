644d47815f1fb21517e7a0eb6187f3f3da6de4b2
htchepannou <htchepannou@expedia.com>
2015-09-06 17:28:44 -0400

    add filter by status

---
+++ src/main/java/com/tchepannou/blog/client/v1/SearchRequest.java
@@ -0,0 +1,50 @@
+package com.tchepannou.blog.client.v1;
+
+import javax.validation.constraints.Pattern;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class SearchRequest {
+    public static final String DEFAULT_STATUS = "all";
+    public static final int DEFAULT_LIMIT = 20;
+
+    private Collection<Long> blogIds = new HashSet<>();
+
+    @Pattern(regexp = "all|draft|published", message="status_invalid")
+    private String status = DEFAULT_STATUS;
+
+    private int limit = DEFAULT_LIMIT;
+    private int offset;
+
+    public void addBlogId (long blogId){
+        blogIds.add(blogId);
+    }
+
+    public Collection<Long> getBlogIds() {
+        return blogIds;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
